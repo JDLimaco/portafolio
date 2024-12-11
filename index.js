44090642c53e61b36064718f88ab31d5a01a1734
JDLimaco <juan_camarena2003@hotmail.com>
2024-12-10 19:27:52 -0500

    Subida de prmiera version estable del portfolio

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 
 function init() {
     // Esperar a que el modal esté completamente visible
+
+    document.getElementById('modalPosPeruAppLecturaBalanza').addEventListener('shown.bs.modal', function () {
+        // Inicializar o actualizar el Swiper cuando el modal esté visible
+        const swiper = new Swiper('#swiperPosPeruAppLecturaBalanza', {
+            effect: 'flip',
+            grabCursor: true,
+            pagination: {
+                el: '.swiper-pagination'
+            },
+            navigation: {
+                prevEl: '.swiper-button-prev',
+                nextEl: '.swiper-button-next'
+            }
+        });
+    });
+
+    document.getElementById('modalPosPeruERP').addEventListener('shown.bs.modal', function () {
+        // Inicializar o actualizar el Swiper cuando el modal esté visible
+        const swiper = new Swiper('#swiperPosPeruERP', {
+            effect: 'flip',
+            grabCursor: true,
+            pagination: {
+                el: '.swiper-pagination'
+            },
+            navigation: {
+                prevEl: '.swiper-button-prev',
+                nextEl: '.swiper-button-next'
+            }
+        });
+    });
+
     document.getElementById('modalSagitarioSystemIntranet').addEventListener('shown.bs.modal', function () {
         // Inicializar o actualizar el Swiper cuando el modal esté visible
         const swiper = new Swiper('#swiperLawnTennisIntranet', {
@@ -18,6 +49,48 @@ function init() {
     document.getElementById('modalSagitarioSystemAplicacionMovil').addEventListener('shown.bs.modal', function () {
         // Inicializar o actualizar el Swiper cuando el modal esté visible
         const swiper = new Swiper('#swiperLawnTennisAplicacionMovil', {
+            effect: 'flip',
+            grabCursor: true,
+            pagination: {
+                el: '.swiper-pagination'
+            },
+            navigation: {
+                prevEl: '.swiper-button-prev',
+                nextEl: '.swiper-button-next'
+            }
+        });
+    });
+    document.getElementById('modalRzServicioExpressSistemaDelivery').addEventListener('shown.bs.modal', function () {
+        // Inicializar o actualizar el Swiper cuando el modal esté visible
+        const swiper = new Swiper('#swiperRzServicioExpressSistemaDelivery', {
+            effect: 'flip',
+            grabCursor: true,
+            pagination: {
+                el: '.swiper-pagination'
+            },
+            navigation: {
+                prevEl: '.swiper-button-prev',
+                nextEl: '.swiper-button-next'
+            }
+        });
+    });
+    document.getElementById('modalRzServicioExpressAPIRest').addEventListener('shown.bs.modal', function () {
+        // Inicializar o actualizar el Swiper cuando el modal esté visible
+        const swiper = new Swiper('#swiperRzServicioExpressAPIRest', {
+            effect: 'flip',
+            grabCursor: true,
+            pagination: {
+                el: '.swiper-pagination'
+            },
+            navigation: {
+                prevEl: '.swiper-button-prev',
+                nextEl: '.swiper-button-next'
+            }
+        });
+    });
+    document.getElementById('modalBilleteraAlertSistemaEmpresarial').addEventListener('shown.bs.modal', function () {
+        // Inicializar o actualizar el Swiper cuando el modal esté visible
+        const swiper = new Swiper('#swiperBilleteraAlertSistemaEmpresarial', {
             effect: 'flip',
             grabCursor: true,
             pagination: {
