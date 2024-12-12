9641513d49ec57611f9c585bd9bdec5824b2256a
JDLimaco <juan_camarena2003@hotmail.com>
2024-12-12 15:51:29 -0500

    Correcion de descripción

--- index.js
+++ index.js
@@ -74,20 +74,6 @@ function init() {
             }
         });
     });
-    document.getElementById('modalRzServicioExpressAPIRest').addEventListener('shown.bs.modal', function () {
-        // Inicializar o actualizar el Swiper cuando el modal esté visible
-        const swiper = new Swiper('#swiperRzServicioExpressAPIRest', {
-            effect: 'flip',
-            grabCursor: true,
-            pagination: {
-                el: '.swiper-pagination'
-            },
-            navigation: {
-                prevEl: '.swiper-button-prev',
-                nextEl: '.swiper-button-next'
-            }
-        });
-    });
     document.getElementById('modalBilleteraAlertSistemaEmpresarial').addEventListener('shown.bs.modal', function () {
         // Inicializar o actualizar el Swiper cuando el modal esté visible
         const swiper = new Swiper('#swiperBilleteraAlertSistemaEmpresarial', {
